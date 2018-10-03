180be4daf150809cbc671a3dfe6e8bcd61089f10
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-10-02 20:06:05 -0700

    Seperated everything and got the MVC functioning

--- Controller.js
+++ Controller.js
@@ -1,4 +1,5 @@
-var amountOfQuestions = 1; 
+var quiz = new Quiz();
+alert(testView);
 
 function submitForm(formElement){
     var id = formElement.id;
@@ -7,53 +8,10 @@ function submitForm(formElement){
 }
 
 function addQuestion(){
+    var amountOfQuestions = quiz.getamountOfQuestions();
     amountOfQuestions++;
-    var quizDiv = document.getElementById("quizDiv");
-    
-    var form = document.createElement("form");
-    form.id = amountOfQuestions;
-    
-    var br = document.createElement("br");
-    form.appendChild(br);
-    
-    var text = document.createTextNode("Question "+amountOfQuestions);
-    form.appendChild(text);
-
-    var br = document.createElement("br");
-    form.appendChild(br);
-    
-    var textarea = document.createElement("textarea");
-    textarea.setAttribute('value', 'Your question goes here');
-    textarea.id = "Question"+amountOfQuestions+"Text";
-    form.appendChild(textarea);
-
-    var br = document.createElement("br");
-    form.appendChild(br);
-    
-
-    for (i = 0; i <4; i++){ 
-        var radiobtn = document.createElement("INPUT");
-        radiobtn.id = "Question"+i;
-        radiobtn.setAttribute('type', 'radio');
-        radiobtn.setAttribute('value', 'Question'+amountOfQuestions+'Answer'+i)
-        radiobtn.setAttribute('name','answers')
-        form.appendChild(radiobtn);
-
-        var input1 = document.createElement("INPUT");
-        input1.id = "Question"+amountOfQuestions+"Answers"+i;
-        input1.setAttribute("type", "html")
-        form.appendChild(input1);
-        var br = document.createElement("br");
-        form.appendChild(br);
-    }
-
-    var btn = document.createElement("INPUT");
-    btn.setAttribute('type', 'button');
-    btn.setAttribute('onclick', 'submitForm(this.form)');
-    btn.setAttribute('value', 'Save This Question')
-    form.appendChild(btn);
-
-    quizDiv.appendChild(form);
+    quiz.setamountOfQuestions(amountOfQuestions);
+    addQuestionView(amountOfQuestions);
 }
 
 /*
--- Model.js
+++ Model.js
@@ -1,20 +1,27 @@
+var test = "hkjdas";
+
 class Quiz{
     constructor(){
         this.amountOfQuestions = 1;
     }
     
-    getamountOfQuestions = function(){
-        return amountOfQuestions;
-    }    
+    getamountOfQuestions(){
+        return this.amountOfQuestions;
+    }
+    
+    setamountOfQuestions(newAmt){
+        this.amountOfQuestions = newAmt;
+    }
 }
 
 class Question{
-        constructor(){
-        this.id;
-        this.questionText;
-        this.answer1;
-        this.answer2;
-        this.answer3;
-        this.correctAnswer;
+        constructor(id, questionText, answer1, answer2, answer3, answer4, correctAnswer){
+        this.id = id;
+        this.questionText = questionText;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.correctAnswer = correctAnswer;
     }
 }
